d66e5f2156f1e9d58af8a2af4cf288eb856e01ee
Sebastian Rodriguez <ply.srh@gmail.com>
2024-10-05 15:06:00 +0000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -13,10 +13,16 @@ function repl() {
     if (command === 'exit') {
       const exitCode = parts[1] ? parseInt(parts[1]) : 0;
       process.exit(exitCode);
-    }
-    
-    if (command === 'echo') {
+    } else if (command === 'echo') {
       console.log(parts.slice(1).join(' '));
+    } else if (command === 'type') {
+      const targetCommand = parts[1];
+
+      if (['echo', 'exit', 'type'].includes(targetCommand)) {
+        console.log(`${targetCommand} is a shell builtin`);
+      } else {
+        console.log(`${targetCommand}: not found`);
+      }
     } else {
       console.log(`${answer}: command not found`);
     }
